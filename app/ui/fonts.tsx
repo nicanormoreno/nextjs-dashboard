fbc5bc9a98ef08b15469bb0d29de96a6a66dd5d2
nicanormoreno <nicanor.moreno@careguideplatform.onmicrosoft.com>
2024-08-15 19:34:01 -0300

    first set

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import {Lusitana, Roboto} from 'next/font/google'
+
+export const font = Roboto({
+    subsets:['latin'],
+    weight:['400']
+})
+
+export const lusitana = Lusitana({
+    subsets:['latin'],
+    weight:['400', '700']
+})
